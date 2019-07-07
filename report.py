f1589fd8739d627f30ae5429e667d20d7e13b9d1
Rile_Cry <rileycriddle@yahoo.com>
2019-07-06 23:07:50 -0500

    0.0.1
    Created the report module for the program.

---
+++ report.py
@@ -0,0 +1,29 @@
+# Module Title: Report
+# Created By: Rile_Cry
+# Created On: 24 June 2019
+# Description: This module is focused on creating a report from the
+# DragPy's simulation. All results from the simulation, if allowed, are
+# recreated in file form as a report.
+
+# Main Class
+
+
+class Report:
+    '''
+    Class: Report
+    Params:
+        None
+    Functions:
+        None
+    '''
+
+    # Class Setup
+
+    def __init__(self):
+        pass
+
+
+# Test
+
+if __name__ == '__main__':
+    rep = Report()
