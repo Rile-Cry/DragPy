ea61f3802fe2e21005f52ea32726c35ad89e7986
Rile_Cry <rileycriddle@yahoo.com>
2019-07-06 22:55:50 -0500

    0.0.0
    This is the initial setup of the program

---
+++ main.py
@@ -0,0 +1,18 @@
+# Program Title: Dragpy
+# Created By: Rile_Cry
+# Created On: 24 June 2019
+# Current Version: 0.0.0
+# Description: Dragpy is a aerodynamic entry simulation program originally
+# built for L'Space Academy Level 1 2019 for atmospheric entry of Titan.
+
+# Main Function
+
+
+def main:
+    pass
+
+
+# Run Condition
+
+if __name__ == '__main__':
+    main()
